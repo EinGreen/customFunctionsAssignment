2839baab7cf818091f846cf95bd3bb81d951912c
Ein_Green <mbananola2002@gmail.com>
2021-03-22 10:42:31 -0700

    started to work on the custom functions assignment, made the [3~

---
+++ app.js
@@ -0,0 +1,13 @@
+// create a Function that takes 2 integers as arguments, and returns the multiplied value of the two numbers
+
+// this function multiplies the 2 numbers in the round brackets together
+// Note: I suck at naming things, so Ima just be lazy with the naming
+function multiply(number, number2) {
+    var multipliedNumber = number * number2;
+    return multipliedNumber;
+}
+
+// this variable will be iterated on the console
+var multiplied = multiply(8, 2);
+console.log(multiplied);
+
